538f51db3aa3d17651baee622b3f8487149ef240
Andrei <andrei.rezende@hotmail.com>
2025-05-27 19:04:09 -0300

    feat: add AvaliacaoFisicaRepository interface

---
+++ src/main/java/com/br/ala_gym_poo2/repository/AvaliacaoFisicaRepository.java
@@ -0,0 +1,7 @@
+package com.br.ala_gym_poo2.repository;
+
+import com.br.ala_gym_poo2.model.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
+}
