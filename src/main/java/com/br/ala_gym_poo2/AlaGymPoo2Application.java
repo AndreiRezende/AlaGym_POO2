2b43cb4d0f7a6c93b7df555c4439772a6f360998
Andrei <andrei.rezende@hotmail.com>
2025-05-26 21:42:37 -0300

    init all commits

---
+++ src/main/java/com/br/ala_gym_poo2/AlaGymPoo2Application.java
@@ -0,0 +1,13 @@
+package com.br.ala_gym_poo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaGymPoo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlaGymPoo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ala_gym_poo2/AlaGymPoo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ala_gym_poo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlaGymPoo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
