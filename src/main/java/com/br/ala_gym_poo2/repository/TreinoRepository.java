4bc2d2afc99cbac4bfbe502e85d225293f4301b9
Andrei <andrei.rezende@hotmail.com>
2025-05-27 19:04:32 -0300

    feat: add TreinoRepository interface

---
+++ src/main/java/com/br/ala_gym_poo2/repository/TreinoRepository.java
@@ -0,0 +1,7 @@
+package com.br.ala_gym_poo2.repository;
+
+import com.br.ala_gym_poo2.model.Treino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TreinoRepository extends JpaRepository<Treino, Long> {
+}
